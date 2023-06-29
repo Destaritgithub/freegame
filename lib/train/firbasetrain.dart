1eb87cc05a7b6ee6fa825df0bcdc4493066e3945
Destaritgithub <alexdesse@gmail.com>
2023-06-29 07:56:30 -0700

    flutter firbase intialize

---
+++ lib/train/firbasetrain.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Trainfirbase extends StatefulWidget {
+  const Trainfirbase({super.key});
+
+  @override
+  State<Trainfirbase> createState() => _TrainfirbaseState();
+}
+
+class _TrainfirbaseState extends State<Trainfirbase> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
